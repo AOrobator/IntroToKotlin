a02600568a6296df1b65d4a9069510e7ff37c201
AndrewOrobator <Orobator.Andrew@gmail.com>
2018-06-17 12:57:13 -0400

    Basic Types

--- src/com/orobator/kotlin/intro/HelloWorld.kt
+++ src/com/orobator/kotlin/intro/HelloWorld.kt
@@ -25,4 +25,24 @@ fun main(args: Array<String>) {
     val pet: String = "Husky" // Can optionally specify the type
     val food = "Tomato" // This is fine as well. The compiler knows this is a String
 
+
+    /////////////////
+    /* Basic Types */
+    /////////////////
+
+    val myDouble: Double = 42.0 // 64 bits wide
+    val myFloat: Float = 42f // 32 bits wide
+
+    val myLong: Long = 42L // 64 bits wide
+    val myInt: Int = 42 // 32 bits wide
+    val myShort: Short = 42 // 16 bits wide
+    val myByte: Byte = 42 // 8 bits wide
+
+    // Everything is an object! Smaller types not subtypes of larger types.
+
+    // val impossible: Long = myInt
+    val castedLong: Long = myInt.toLong()
+    println("Casted Long = $castedLong")
+
+    // Numbers are represented as primitives unless generics or nullability is involved.
 }
