820ab91f4c73f7bfbd1eb6a685517f4b3014d9b8
AndrewOrobator <Orobator.Andrew@gmail.com>
2018-06-17 12:11:37 -0400

    val vs. var

--- src/com/orobator/kotlin/intro/HelloWorld.kt
+++ src/com/orobator/kotlin/intro/HelloWorld.kt
@@ -6,4 +6,11 @@ package com.orobator.kotlin.intro
 fun main(args: Array<String>) {
     val scope = "World"
     println("Hello $scope!") // Kotlin supports String interpolation
+
+    /* val vs var */
+    val name = "Andrew" // read-only variable
+    var age = 24 // read & write variable
+
+    age = 25 // Allowed, because this is a var
+    // name = "Dan" // Not allowed because this is a val
 }
