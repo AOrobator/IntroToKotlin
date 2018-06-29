ac69b9fee695b5ee649510f0f88558def8db8f1f
AndrewOrobator <Orobator.Andrew@gmail.com>
2018-06-29 06:44:19 -0400

    Added for loops

--- src/com/orobator/kotlin/intro/L3_ControlFlow.kt
+++ src/com/orobator/kotlin/intro/L3_ControlFlow.kt
@@ -59,4 +59,43 @@ fun main(args: Array<String>) {
         x is String -> println("Case 2")
         animal == "Husky" -> println("woof")
     } // Don't need else because this isn't an expression
+
+
+    // For loops
+    // Use ranges to iterate through numbers
+    for (i in 0..10) {
+        print("*")
+    }
+
+    println()
+
+    for (i in 15 downTo 0 step 3) {
+        when (i) {
+            0 -> print(0)
+            else -> print("$i, ")
+        }
+    }
+
+    println()
+
+    val list = listOf(1, 2, 3, 4, 5)
+    for (int in list) { // Iterates through anything that provides an Iterator
+        println(int)
+    }
+
+    // Can be on a single line
+    for (myInt: Int in list) print(myInt)
+
+    println()
+
+    for (i in list.indices) print(list[i])
+
+    println()
+
+    for ((index, value) in list.withIndex()) {
+        println("Element at index $index is $value")
+    }
+
+    list.forEach { println("For each $it") }
+
 }
