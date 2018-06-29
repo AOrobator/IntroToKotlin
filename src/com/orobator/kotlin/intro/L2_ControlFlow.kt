772d1dff5ae2c88535575f3b0d121b2d77c17fa0
AndrewOrobator <Orobator.Andrew@gmail.com>
2018-06-28 21:41:41 -0400

    Added default arguments for functions

--- src/com/orobator/kotlin/intro/L2_ControlFlow.kt
+++ src/com/orobator/kotlin/intro/L2_ControlFlow.kt
@@ -12,6 +12,14 @@ fun main(arguments: Array<String>) {
 
     // Named parameters allow any param order
     println("2 - 3 = ${subtract(y = 3, x = 2)}")
+
+    println()
+    printDefaults(100)
+    printDefaults()
+
+    println()
+
+    printDefaults2(3)
 }
 
 // fun keyword used to start functions
@@ -19,7 +27,7 @@ fun main(arguments: Array<String>) {
 // return type at the end of the declaration
 // Unit doesn't return anything, similar to Void in Java.
 fun maybePrintLength(any: Any): Unit {
-    if (any is String) { // is: type check operator
+    if (any is String) { // is: type check operator (instanceof in Java)
         println("$any's length is ${any.length}") // Any is smart casted to a string
     } else {
         println("$any isn't a String")
@@ -30,3 +38,13 @@ fun maybePrintLength(any: Any): Unit {
 fun add(x: Int, y: Int): Int = x + y
 
 fun subtract(x: Int, y: Int) = x - y // Return type is inferred by compiler
+
+// Default Arguments allow for less overloaded functions
+fun printDefaults(foo: Int = 42) {
+    println("printDefaults(foo = $foo)")
+}
+
+// For multiple arguments, the defaults are usually last, otherwise you're forced to use named params
+fun printDefaults2(foo: Int, bar: String = "Bar", baz: Double = 12.0) {
+    println("printDefaults2(foo = $foo, bar = $bar, baz = $baz)")
+}
