1ab3d16b7ee251cb4e188d1af238c5297b0e4b08
AndrewOrobator <Orobator.Andrew@gmail.com>
2018-07-08 21:11:28 -0700

    Added inner classes

---
+++ src/com/orobator/kotlin/intro/lesson14/L14_InnerClasses.kt
@@ -0,0 +1,22 @@
+package com.orobator.kotlin.intro.lesson14
+
+// Inner Classes
+
+class WishlistAdapter {
+    val listItems = listOf("Shoes", "Belt", "Tie")
+
+    // Classes can be nested in other classes
+    class WishlistItemViewHolder {
+        // Nested classes can't access the outer class's members by default
+        // Can't access listItems, opposite of Java
+    }
+
+    // A class may be marked as inner to be able to access members of
+    // outer class. Inner classes hold onto a reference of the outer class
+    inner class ItemViewHolder {
+        init {
+            // Can qualify reference to "this" with @ClassName.
+            println(this@WishlistAdapter.listItems.size)
+        }
+    }
+}
