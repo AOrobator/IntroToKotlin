d2ee94d51dad081625f0a7725d7407a2ed9431f4
AndrewOrobator <Orobator.Andrew@gmail.com>
2018-07-31 23:17:09 -0400

    Added enums lab & review

---
+++ src/com/orobator/kotlin/intro/lesson12/labs/L12_Enums_Lab0.kt
@@ -0,0 +1,14 @@
+package com.orobator.kotlin.intro.lesson12.labs
+
+// Declare a class Steak that extends class Meat
+// Declare an enum Doneness with the following values:
+// - Raw, Rare, Medium, Well Done, Charred
+
+// Steak should have a property doneness that is readable by everyone,
+// but can only be written to by instances of Steak. Steaks start out raw.
+
+// Define a method cook() that increases the Doneness level by 1,
+// until it's Charred
+
+// Create a main function that "cooks" a steak 10 times and prints out the
+// doneness after each "cook".
---
+++ src/com/orobator/kotlin/intro/lesson12/labs/Review.kt
@@ -0,0 +1,15 @@
+package com.orobator.kotlin.intro.lesson12.labs
+
+// Review
+
+// 1. What are some of the differences between Kotlin and Java?
+
+// 2. What's the difference between a Java field and a Kotlin property?
+
+// 3. Write a function isMultipleOfThree that takes an Int and returns true if
+//    it's a multiple of three. False otherwise
+
+// 4. Create a list of numbers from 1 to 10 called firstList. Create secondList
+//    based on firstList. secondList should only have odd numbers. Create
+//    thirdList based on secondList. thirdList should be the squares of
+//    secondList.
