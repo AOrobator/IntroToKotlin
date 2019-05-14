dedddf26c23f35b2202dc30fbe60bf15c8f08291
AndrewOrobator <Orobator.Andrew@gmail.com>
2019-05-14 00:00:47 -0700

    L26: Added launch description

--- lessons/src/main/com/orobator/kotlin/intro/lesson26/L26_Coroutines_2_Builders.kt
+++ lessons/src/main/com/orobator/kotlin/intro/lesson26/L26_Coroutines_2_Builders.kt
@@ -4,6 +4,12 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+// Normal functions can't call suspend functions
+
+// We get into the land of coroutines and suspending functions using
+// coroutine builders.
+
+// Launch - One of the most prominent coroutine builders. Fire & forget.
 fun main() {
     GlobalScope.launch { // launch a new coroutine in background and continue
         delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
@@ -15,6 +21,5 @@ fun main() {
 
 // Lab 0
 
-// Coroutine builders are doorways into coroutine land
 // break down launch, runBlocking, async?
 // Structured concurrency, scope, Job
