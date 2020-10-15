8f179b11892af90a2a439977e2d9c9f7af43d562
Andrew Orobator <Orobator.Andrew@gmail.com>
2020-10-15 01:06:16 -0400

    Added Labs for Lesson 25

---
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson25/labs/L25_Interop_Lab.kt
@@ -0,0 +1,12 @@
+package com.orobator.kotlin.intro.lesson25.labs
+
+// Create a class in Java called DogPojo. It should have a private field name
+// that's a string. name should have a getter and a setter. Instantiate DogPogo
+// from Kotlin. Note how you can call the getter and setter in the traditional
+// Java way as well as Kotlin's property syntax.
+
+// What is one the biggest difference between Java and Kotlin's exception
+// handling? How does this difference affect interoperability?
+
+// Create an example of a platform type throwing a runtime error when consumed
+// from Kotlin.
---
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson25/solutions/DogPojo.java
@@ -0,0 +1,17 @@
+package com.orobator.kotlin.intro.lesson25.solutions;
+
+public class DogPojo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return null;
+    }
+}
