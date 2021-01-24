5e7eb148a6c4fc273dd3a4601ded8adb1072d0e0
Andrew Orobator <Orobator.Andrew@gmail.com>
2021-01-24 17:53:47 -0500

    Fleshed out Lab 1 solution, still need launch example

--- src/main/kotlin/com/orobator/kotlin/intro/lesson26/solutions/L26_Coroutines_Lab1_Solution.kt
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson26/solutions/L26_Coroutines_Lab1_Solution.kt
@@ -1,20 +1,64 @@
 package com.orobator.kotlin.intro.lesson26.solutions
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-// Lab on builders
+// Lab 1: Coroutine Builders
 
-// have them use async to fetch 2 things from fake network concurrently.
-fun concurrentFetch() = GlobalScope.launch {
+// Q: What are coroutine builders used for? (Don't say building coroutines lol)
 
+// A suspending function can only be called from another suspending function or
+// a coroutine. In order to bridge the gap between non-suspending functions and
+// suspending functions, we can use a coroutine builder such as launch where the
+// body of the lambda passed in is a coroutine that can call suspending
+// functions.
+
+
+// Q: Given the AccountsApi, print out the checkings and savings balances on one
+// line with the format:
+
+// Checkings Balance = A, Savings Balance = B
+
+// In displayBalancesSerially() you should make sure that the fetch to get the
+// checkings balance completes before the call to get the savings balance is
+// made.
+
+// In displayBalancesConcurrently() you should make sure that the calls to get
+// the checkings and savings balances are running at the same time. Your code
+// should make sure that both calls have completed before printing the result.
+
+// Both functions should have identical output of:
+// Checkings Balance = $420, Savings balance = $720
+
+object AccountsApi {
+    suspend fun getCheckingsBalance(): String {
+        delay(1000L)
+        return "$420"
+    }
+
+    suspend fun getSavingsBalance(): String {
+        delay(1000L)
+        return "$720"
+    }
 }
 
-suspend fun CoroutineScope.concurrentFetch(userId1: String, userId2: String) {
+fun main() {
+    // This can be solved without modifying main()
+    displayBalancesSerially()
+    displayBalancesConcurrently()
+}
 
+fun displayBalancesSerially() = runBlocking {
+    val checkings = AccountsApi.getCheckingsBalance()
+    val savings = AccountsApi.getSavingsBalance()
+
+    println("Checkings Balance = $checkings, Savings balance = $savings")
+}
+
+fun displayBalancesConcurrently() = runBlocking {
+    val checkings = async { AccountsApi.getCheckingsBalance() }
+    val savings = async { AccountsApi.getSavingsBalance() }
+
+    println("Checkings Balance = ${checkings.await()}, Savings balance = ${savings.await()}")
 }
 
 // Launch example - maybe firing off an analytics event?
-
-// Run blocking example
