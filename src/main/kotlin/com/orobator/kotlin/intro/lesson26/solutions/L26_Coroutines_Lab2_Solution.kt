7f4ff82e79e92395a4549f0d615215cc21e2f037
Andrew Orobator <Orobator.Andrew@gmail.com>
2021-01-30 16:08:37 -0500

    Using callback

--- src/main/kotlin/com/orobator/kotlin/intro/lesson26/solutions/L26_Coroutines_Lab2_Solution.kt
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson26/solutions/L26_Coroutines_Lab2_Solution.kt
@@ -11,8 +11,20 @@ interface ReadTextFileCallback {
 fun readFile(path: String, callback: ReadTextFileCallback) {
     val currentTime = System.currentTimeMillis().toInt()
     if (currentTime % 2 == 0) {
-        callback.onSuccess(path+ "File Contents")
+        callback.onSuccess("$path File Contents")
     } else {
         callback.onError(IOException())
     }
+}
+
+fun main() {
+    readFile("test-path", object : ReadTextFileCallback {
+        override fun onSuccess(text: String) {
+            println("Received $text")
+        }
+
+        override fun onError(exception: Exception) {
+            println("Got error $exception")
+        }
+    })
 }
