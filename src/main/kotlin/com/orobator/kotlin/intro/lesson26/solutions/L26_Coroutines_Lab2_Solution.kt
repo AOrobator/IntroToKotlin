4ed661c47ccf11b3df73ffb80404a18e93484c70
Andrew Orobator <Orobator.Andrew@gmail.com>
2021-01-30 16:04:53 -0500

    Created callback

--- src/main/kotlin/com/orobator/kotlin/intro/lesson26/solutions/L26_Coroutines_Lab2_Solution.kt
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson26/solutions/L26_Coroutines_Lab2_Solution.kt
@@ -1,3 +1,18 @@
 package com.orobator.kotlin.intro.lesson26.solutions
 
+import java.io.IOException
+
 // Given an API with callbacks convert it to use coroutines
+interface ReadTextFileCallback {
+    fun onSuccess(text: String)
+    fun onError(exception: Exception)
+}
+
+fun readFile(path: String, callback: ReadTextFileCallback) {
+    val currentTime = System.currentTimeMillis().toInt()
+    if (currentTime % 2 == 0) {
+        callback.onSuccess(path+ "File Contents")
+    } else {
+        callback.onError(IOException())
+    }
+}
