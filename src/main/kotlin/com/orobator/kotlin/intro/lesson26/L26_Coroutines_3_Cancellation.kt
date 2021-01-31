50d4227a4e7f09a4eebaf2dea1e91100bdbc0b46
Andrew Orobator <Orobator.Andrew@gmail.com>
2021-01-31 16:33:06 -0500

    ideated structured concurrency

--- src/main/kotlin/com/orobator/kotlin/intro/lesson26/L26_Coroutines_3_Cancellation.kt
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson26/L26_Coroutines_3_Cancellation.kt
@@ -2,8 +2,36 @@ package com.orobator.kotlin.intro.lesson26
 
 // Structured concurrency
 
+// Compare to thread.join(), tracking all launched threads and having to manage them
+
+// Instead use scopes. coroutine builders (examples) give your coroutines a scope to run in
+
+// lambda with receiver type as parameter to coroutine builder
+
+// mention GlobalScope
+
+///////////////
+// Benefits
+
+// The scope is generally responsible for child coroutines, and their lifetime
+// is attached to the lifetime of the scope.
+
+// The scope can automatically cancel child coroutines if something goes wrong
+// or if a user simply changes their mind and decides to revoke the operation.
+
+// The scope automatically waits for completion of all the child coroutines.
+// Therefore, if the scope corresponds to a coroutine, then the parent coroutine
+// does not complete until all the coroutines launched in its scope are complete.
 
 
-// Jobs,
+// Compare cancellation of GlobalScope with cancellation of other scopes
+
+//////////
+// Jobs
+
+// coroutine builders usually return jobs (runBlocking is an exception)
+// job.join()
+
 // Cancellation,
+// lab exploring cancellation
 // Scoping (SupervisorScope)
