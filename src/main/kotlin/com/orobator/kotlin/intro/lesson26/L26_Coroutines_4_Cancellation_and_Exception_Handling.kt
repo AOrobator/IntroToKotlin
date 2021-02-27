583e0c45aaa1efd752f0ae39efc635f5906a50e5
Andrew Orobator <Orobator.Andrew@gmail.com>
2021-02-27 18:23:28 -0500

    Coroutine cancellation & exceptions ideation

---
+++ src/main/kotlin/com/orobator/kotlin/intro/lesson26/L26_Coroutines_4_Cancellation_and_Exception_Handling.kt
@@ -0,0 +1,15 @@
+package com.orobator.kotlin.intro.lesson26
+
+//////////////////////////////////////
+// Cancellation & Exception handling
+
+// See https://kotlinlang.org/docs/exception-handling.html
+
+// Cancellation is cooperative
+  // - show example of child not cancelling when parent cancels
+// Review exception behavior for CoroutineScope.launch
+// SupervisorJob vs regular (parent/child relationship)
+// Exception behavior for async
+// CoroutineExceptionHandler
+
+// Lab - Write code that does cooperative cancellation. Maybe compute fib(n)
--- src/main/kotlin/com/orobator/kotlin/intro/lesson26/L26_Coroutines_N_Structured_Concurrency.kt
+++
@@ -1,8 +0,0 @@
-package com.orobator.kotlin.intro.lesson26
-
-//////////////////////////
-// Structured concurrency
-
-
-// Parent Scope waits for children to complete
-
